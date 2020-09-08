b5648f2a077e8c9c19bcc1e6d07c764622d7e66a
Hector Remedios <hectoremedio90@gmail.com>
2020-09-07 20:15:18 -0500

    Styles added for the principal section

--- JS/navBar_funtion.js
+++ JS/navBar_funtion.js
@@ -1,16 +1,16 @@
 
 function openContents(evt, info) {
     var i, tabcontent, tablinks;
-        tabcontent = document.getElementById("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementById("tablink");
-        for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(info).style.display = "block";
-        evt.currentTarget.className += " active";
-     }
-document.getElementById("defaultOpen").click();
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(info).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click(event);
   
